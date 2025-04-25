a19bb381e4b7dbfab06848a531451b3a8698377d
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-04-25 04:25:14 -0300

    Updating Video Call Interface

--- lib/screens/video_call_page.dart
+++ lib/screens/video_call_page.dart
@@ -1,76 +1,191 @@
 import 'package:flutter/material.dart';
 
-class VideoCallPage extends StatelessWidget {
+class VideoCallPage extends StatefulWidget {
   const VideoCallPage({super.key});
+
+  @override
+  State<VideoCallPage> createState() => _VideoCallPageState();
+}
+
+class _VideoCallPageState extends State<VideoCallPage> {
+  Offset _overlayPosition = const Offset(230, 320); // posição inicial do mascote
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF3E8F9),
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            const SizedBox(height: 30),
-            const Text(
-              "Dra. Mariana",
+            Column(
+              children: [
+                const SizedBox(height: 40),
+                _buildHeader(),
+                const SizedBox(height: 40),
+                Center(child: _buildAvatar()),
+                const Spacer(),
+                _buildControls(),
+                const SizedBox(height: 40),
+              ],
+            ),
+            // avatar flutuante e arrastável
+            Positioned(
+              left: _overlayPosition.dx,
+              top: _overlayPosition.dy,
+              child: GestureDetector(
+                onPanUpdate: (details) {
+                  setState(() {
+                    _overlayPosition += details.delta;
+                  });
+                },
+                child: _buildOverlayAvatar(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+  return Container(
+    margin: const EdgeInsets.symmetric(horizontal: 24),
+    padding: const EdgeInsets.all(16),
+    decoration: BoxDecoration(
+      color: Colors.white.withOpacity(0.8),
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black12,
+          blurRadius: 10,
+          offset: Offset(0, 4),
+        ),
+      ],
+    ),
+    child: Row(
+      children: [
+        CircleAvatar(
+          radius: 28,
+          backgroundImage: AssetImage('assets/avatar_psicologa.png'),
+        ),
+        const SizedBox(width: 16),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              "Dra. Gabriela Lopes",
               style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
                 color: Color(0xFF5E348F),
               ),
             ),
-            const SizedBox(height: 8),
-            const Text(
-              "Conectando...",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-            ),
-            const SizedBox(height: 30),
-            Center(
-              child: Container(
-                width: 160,
-                height: 160,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: const Color(0xFFD1C4E9),
-                  border: Border.all(
-                    color: const Color(0xFF5E348F),
-                    width: 4,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 8,
-                      offset: Offset(0, 4),
-                    ),
-                  ],
-                ),
-                child: const Center(
-                  child: Icon(
-                    Icons.person,
-                    size: 80,
-                    color: Colors.white,
+            SizedBox(height: 4),
+            Row(
+              children: [
+                SizedBox(
+                  width: 12,
+                  height: 12,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2,
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
                   ),
                 ),
-              ),
+                SizedBox(width: 6),
+                Text(
+                  "Conectando...",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
             ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _buildCallButton(Icons.mic, const Color(0xFF9575CD)),
-                  _buildCallButton(Icons.call_end, Colors.red),
-                  _buildCallButton(Icons.videocam, const Color(0xFF9575CD)),
-                ],
-              ),
-            ),
-            const SizedBox(height: 40),
           ],
         ),
+      ],
+    ),
+  );
+}
+
+
+  Widget _buildAvatar() {
+  return Stack(
+    alignment: Alignment.bottomLeft,
+    children: [
+      Container(
+        width: 240,
+        height: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24),
+          image: const DecorationImage(
+            image: AssetImage('assets/avatar_psicologa.png'),
+            fit: BoxFit.cover,
+          ),
+          boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(66, 255, 253, 253),
+              blurRadius: 20,
+              offset: Offset(0, 10),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        margin: const EdgeInsets.all(12),
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 6),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.6),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: const Text(
+          'Chamada em andamento',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+
+  Widget _buildOverlayAvatar() {
+    return Container(
+      width: 150,
+      height: 200,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.5),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+            offset: Offset(2, 4),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.asset(
+          'assets/cat_icon.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildControls() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildCallButton(Icons.mic, const Color(0xFF9575CD)),
+          _buildCallButton(Icons.call_end, Colors.red),
+          _buildCallButton(Icons.videocam, const Color(0xFF9575CD)),
+        ],
       ),
     );
   }
@@ -80,19 +195,19 @@ class VideoCallPage extends StatelessWidget {
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         color: color,
-        boxShadow: [
+        boxShadow: const [
           BoxShadow(
             color: Colors.black26,
-            blurRadius: 6,
-            offset: Offset(2, 2),
+            blurRadius: 10,
+            offset: Offset(2, 4),
           ),
         ],
       ),
-      padding: const EdgeInsets.all(18),
+      padding: const EdgeInsets.all(20),
       child: Icon(
         icon,
         color: Colors.white,
-        size: 30,
+        size: 28,
       ),
     );
   }
